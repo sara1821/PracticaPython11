16040d9709bab24a5e4ac566244c2ed90b0cca52
sara1821 <s.gonzalez@coadvil.edu.co>
2025-03-04 11:25:19 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+horas_vuelo = [11.5, 13.3, 15, 15.4, 16.2, 18, 17.3, 20, 15.4, 14]
+promedio = sum(horas_vuelo) / len(horas_vuelo)
+print(f"El promedio de horas de vuelo en los últimos 10 días es: {promedio:.2f} horas")
